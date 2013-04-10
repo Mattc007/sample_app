75f95e4e7d735008fa6036f21fb4a69793d099ea
Matt Catrett <mcatrett@turbosquid.com>
2013-04-09 21:01:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8645a6bf242e25f54c003170c9acb625bf72a15e9a008e1b43a0e52145014e6d287eb98aacedba247b18d82d0ce739b0a99d657c8e3d99770851a7a98d0258ff'
